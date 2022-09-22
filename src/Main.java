495084a1478f376ee0df93055f8f2154968156c7
kchzhao <kamila.chzhao@gmail.com>
2022-09-21 23:18:30 -0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
         var cat = 3.6;
         var paper = 763789;
 
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
